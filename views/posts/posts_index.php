6325eb8df0d75c780dcf66aede324e4e03046005
maardz <moldermarge@gmail.com>
2014-11-03 17:49:24 +0200

    Add tag fix

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,15 +1,15 @@
 <?foreach($posts as $post):?>
-<?foreach ($tags[$post['post_id']]as $tag):?>
-        <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a>
-<?endforeach?>
+
 <div class="span8">
     <h1><a href="<?BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a></h1>
     <p><?=$post['post_text']?></p>
     <div>
         <span class="badge badge-success"><?=$post['post_created']?>4</span>
         <?foreach($tags[$post['post_id']] as $tag):?>
-            <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a>
+            <a href="tags/view/<?= $tag?>"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a>
         <? endforeach?>
+
+        
     
 </div>
 </div>
